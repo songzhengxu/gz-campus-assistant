dac227b24fdf024748f5bb8f5351fb80fb45a0a5
zhengxv song <zhengxv song>
2017-01-15 01:08:12 +0800

    edit

--- app.js
+++ app.js
@@ -1,74 +1,68 @@
 //app.js
 var API_URL = "https://wx.fanweimei.com/wp-content/plugins/PHP_wx_login/login.php";
+let utils = require('utils/util.js');
 App({
-  onLaunch: function () {
-    
-    //获取用户openid 进行默认登陆
-    console.log("iv");
-    wx.login({//login流程
-      success: function (res) {//登录成功
-        if (res.code) {
-          var code = res.code;
-          wx.getUserInfo({//getUserInfo流程
-            success: function (res2) {//获取userinfo成功
-              var encryptedData = res2.encryptedData;
-              var iv = res2.iv;
-              //请求自己的服务器
-              Login(code, encryptedData, iv);
-            }
+  login: function(code, encryptedData, iv) {
+    //创建一个dialog
+    wx.showToast({
+      title: '正在登录...',
+      icon: 'loading',
+      duration: 10000
+    });
+    //请求服务器
+    wx.request({
+      url: API_URL,
+      data: {
+        code: code,
+        encryptedData: encryptedData,
+        iv: iv
+      },
+      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      header: {
+        'content-type': 'application/json'
+      }, // 设置请求的 header
+      success: function (res) {
+        // success
+        wx.hideToast();
+        this.globalData.userInfo = res;
+        var openId=res.data.openId;
+        this.globalData.userInfo.Basic = utils.base64_encode( openId+ ':' + '123456');
+        if (res) {
+          wx.showToast({
+            title: '登陆成功',
+            icon: 'success',
+            duration: 2000
           })
-
-        } else {
-          console.log('获取用户登录态失败！' + res.errMsg)
         }
-      }
+      }.bind(this)
     })
+  },
+  onLaunch: function () {
+    var that=this;
+    if (!this.globalData.userInfo) {
+      wx.login({//login流程
+        success: function (res) {//登录成功
+          if (res.code) {
+            var code = res.code;
+            wx.getUserInfo({//getUserInfo流程
+              success: function (res2) {//获取userinfo成功
+                var encryptedData = res2.encryptedData;
+                var iv = res2.iv;
+                //请求自己的服务器
+                that.login(code, encryptedData, iv);
+              }
+            })
+
+          } else {
+            console.log('获取用户登录态失败！' + res.errMsg)
+          }
+        }
+      })
+    }
   },
   globalData: {
     userInfo: null
-  },
+  }
+
+
 })
-
-function Login(code, encryptedData, iv) {
-  //console.log('code='+code+'&encryptedData='+encryptedData+'&iv='+iv);
-  //创建一个dialog
-  wx.showToast({
-    title: '正在登录...',
-    icon: 'loading',
-    duration: 10000
-  });
-  //请求服务器
-  wx.request({
-    url: API_URL,
-    data: {
-      code: code,
-      encryptedData: encryptedData,
-      iv: iv
-    },
-    method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
-    header: {
-      'content-type': 'application/json'
-    }, // 设置请求的 header
-    success: function (res) {
-      // success
-      wx.hideToast();
-      console.log('服务器返回' + res.data);
-      if (res.data) {
-        wx.showToast({
-          title: '登陆成功',
-          icon: 'success',
-          duration: 2000
-        })
-      }
-
-
-    },
-    fail: function () {
-      // fail
-      // wx.hideToast();
-    },
-    complete: function () {
-      // complete
-    }
-  })
-}
--- pages/my/my.js
+++ pages/my/my.js
@@ -3,6 +3,19 @@ Page({
   data:{},
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
+    wx.request({
+    url: 'https://wx.fanweimei.com/OAuth2/', //仅为示例，并非真实的接口地址
+    data: {
+      x: '' ,
+      y: ''
+    },
+    header: {
+        'content-type': 'application/json'
+    },
+    success: function(res) {
+      console.log(res.data)
+    }
+})
   },
   onReady:function(){
     // 页面渲染完成
