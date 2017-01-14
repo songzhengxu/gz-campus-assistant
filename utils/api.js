4e8b15afabfd93601adb4d04dcaf7020f2122cb7
zhengxv song <zhengxv song>
2017-01-15 02:49:03 +0800

    eidt

--- utils/api.js
+++ utils/api.js
@@ -3,7 +3,6 @@ class Api {
     //获取列表, _this只传入的页面对象，因为需要更改isLoading
     getList(obj, _this) {
         return new Promise((resolve, reject) => {
-            console.log("开始请求列表");
 
             //如果正在加载中或者数据已加载完，则直接返回，目前无效，scroll-view组的bindscrolltolower存在wenw在bug
             if (_this.data.isComplete || _this.data.isLoading) {
@@ -23,9 +22,7 @@ class Api {
             options.isShowToast = obj.isShowToast?obj.isShowToast:true;
             options.time = obj.time?obj.time:0;
             options.param = obj.param?obj.param:{};
-            options.url = obj.url;
-            console.log(options.isShowToast);
-            console.log("请求的地址是"+options.url);            
+            options.url = obj.url;            
             if (options.isShowToast) {
                 wx.showToast({
                     title: "加载中...",
